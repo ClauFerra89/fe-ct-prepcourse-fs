740638be7be0717b6bde4036582bbddc4bbc120b
ClaudiaFerrari89 <claudia.vferrari89@gmail.com>
2023-08-14 19:17:11 -0300

    Mayor menos que...

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,25 +5,63 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   if (x == y) {
+      console.log("Son iguales");
+      return (true); }
+      else {
+         return (false)
+      }
+   }
+
+   sonIguales("yy","yyy")
+
+
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1.length == str2.length) {
+      console.log("Misma Longitud");
+      return (true); 
+   } else {
+      return (false)
+   }
 }
+
+tienenMismaLongitud("Clau","Hideaka")
+
+
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90) {
+      console.log("Menor que Noventa");
+      return (true);
+   } else {
+      return (false)
+   }
 }
+
+menosQueNoventa(75)
+
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num > 50) {
+      return (true);
+   } else {
+      return (false);
+   }
 }
+
+mayorQueCincuenta(33)
+
+
 
 function esPar(num) {
    // Retorna true si "num" es par.
